01a16708a63ed1171fe8b14bbdacf30af54fdc68
suren-dombaev <50293948+suren-dombaev@users.noreply.github.com>
2019-08-07 23:51:20 +0400

    Add files via upload

---
+++ js/library.js
@@ -0,0 +1,50 @@
+let hogeban={
+    name:"Poxos",
+    surname:"Poxosyan",
+
+}
+let srtaban={
+    name:"Vardges",
+    surname:"Vardanyan",
+
+}
+let hogebuj={
+    name:"Artur",
+    surname:"Galstyan",
+
+}
+let virabuj={
+    name:"Artak",
+    surname:"Baxdasaryan"
+}
+
+
+let patient1={
+    name:"Gagik",
+    surname:"Baxdasaryan",
+    doctor:srtaban
+}
+let patient2={
+    name:"Tigran",
+    surname:"Karapetyan",
+    doctor:hogebuj
+}
+let patient3={
+    name:"Artak",
+    surname:"Asatryan",
+    doctor:hogeban
+
+}
+let patient4={
+    name:"Vardan",
+    surname:"Vardanyan",
+    doctor: virabuj,
+
+}
+
+
+let patients=[patient1,patient2,patient3];
+let doctors=[hogeban,hogebuj,srtaban,virabuj];
+
+
+
